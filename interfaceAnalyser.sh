cf184913e0f23b1fd15b51b55415b5d963b750c5
elpipasp <113988540+elpipasp@users.noreply.github.com>
2023-12-20 18:25:43 +0000

    interfaceAnalyser.sh

--- interfaceAnalyser.sh
+++ interfaceAnalyser.sh
@@ -1,1 +1,1 @@
-/opt/software/rosetta/2021.16.61629/main/source/bin/rosetta_scripts.static.linuxgccrelease @interfaceAnalyzer.options -in:file:s *c.*pdb  > InterfaceAnalyis.log
+/opt/software/rosetta/2021.16.61629/main/source/bin/rosetta_scripts.static.linuxgccrelease @interfaceAnalyzer.options -in:file:s *c.*pdb > interfaceAnalyzer.log
