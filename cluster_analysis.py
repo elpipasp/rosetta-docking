1dc9a02842efd1fa33d021887bbd533133207810
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-01-05 20:42:35 +0000

    cluster_analysis.py

--- cluster_analysis.py
+++ cluster_analysis.py
@@ -27,18 +27,17 @@ def calculate_p_values(file_names, cluster_names, column_index, condition_column
     return p_values
 
 # Function to add p-value brackets and annotations
-def add_p_value_brackets(ax, x1, x2, y, p_value, bracket_height=0.02):
+def add_p_value_brackets(ax, x1, x2, y, p_value, bracket_height=0.02, text_y_offset=0.1, fontsize=14):
     # Calculate bracket height based on the range of the y-axis
     bracket_height *= (ax.get_ylim()[1] - ax.get_ylim()[0])
+    text_y_offset *= bracket_height
     ax.plot([x1, x1], [y, y + bracket_height], color='black', lw=1.5)  # Left vertical line
     ax.plot([x1, x2], [y + bracket_height, y + bracket_height], color='black', lw=1.5)  # Horizontal line
     ax.plot([x2, x2], [y, y + bracket_height], color='black', lw=1.5)  # Right vertical line
-    p_text = f"{p_value:.2e}" if p_value < 0.2 else f"{p_value:.4f}"
-    ax.text((x1 + x2) / 2, y + bracket_height, p_text, ha='center', va='top', color='black')
 
     # Format p-value text
-    p_text = f"{p_value:.2e}" if p_value < 0.2 else f"{p_value:.4f}"
-    ax.text((x1 + x2) / 2, y + bracket_height, p_text, ha='center', va='top', color='black')
+    p_text = f"p={p_value:.2e}" if p_value < 0.001 else f"p={p_value:.4f}"
+    ax.text((x1 + x2) / 2, y + bracket_height - text_y_offset, p_text, ha='center', va='bottom', color='black', fontsize=fontsize)
 
 # Function to plot data with p-values
 def plot_data_with_p_values(ax, column_index, ylabel, is_first_plot, is_last_plot, label_text, condition_column_name, condition_value, total_score_condition, y_coordinates):
@@ -47,7 +46,7 @@ def plot_data_with_p_values(ax, column_index, ylabel, is_first_plot, is_last_plo
         data = load_data(file_name, column_index, condition_column_name, condition_value, total_score_condition)
         x_position = position
         parts = ax.violinplot([data], positions=[x_position], showmedians=True, showextrema=False)
-        color_mapping = {'2': 'forestgreen', '4': 'crimson', '1': 'grey', '5': 'cyan'}
+        color_mapping = {'2': 'grey', '4': 'cyan', '1': 'forestgreen', '5': 'magenta'}
         color = color_mapping.get(label, 'black')
         for pc in parts['bodies']:
             pc.set_facecolor(color)
@@ -57,13 +56,13 @@ def plot_data_with_p_values(ax, column_index, ylabel, is_first_plot, is_last_plo
         parts['cmedians'].set_edgecolor('black')
         y_limits.append((data.min(), data.max()))
 
-    ax.set_ylabel(ylabel, fontsize=14, fontweight='bold', labelpad=5)
+    ax.set_ylabel(ylabel, fontsize=20, fontweight='bold', labelpad=5)
     ax.set_xticks(tick_positions)
-    ax.set_xticklabels(tick_labels, fontsize=14)
-    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.set_xticklabels(tick_labels, fontsize=16)
+    ax.tick_params(axis='both', which='major', labelsize=16)
     ax.xaxis.grid(True, linestyle='--', alpha=0.7)
     ax.set_axisbelow(True)
-    ax.text(0.999, 0.009, label_text, transform=ax.transAxes, fontsize=18, fontweight='bold', ha='right', va='bottom')
+    ax.text(0.999, 0.009, label_text, transform=ax.transAxes, fontsize=16, fontweight='bold', ha='right', va='bottom')
     if not is_first_plot:
         ax.yaxis.labelpad = 10
 
@@ -74,8 +73,8 @@ def plot_data_with_p_values(ax, column_index, ylabel, is_first_plot, is_last_plo
     upper_ax = ax.twiny()
     upper_ax.set_xlim(ax.get_xlim())
     upper_ax.set_xticks(tick_positions)
-    upper_ax.set_xticklabels([f"{load_data(file_name, column_index, condition_column_name, condition_value, total_score_condition).count()}" for file_name in file_names], fontsize=14, ha='center', rotation=0)
-    upper_ax.set_xlabel("Population", fontsize=16, fontweight='bold', labelpad=14)
+    upper_ax.set_xticklabels([f"{load_data(file_name, column_index, condition_column_name, condition_value, total_score_condition).count()}" for file_name in file_names], fontsize=16, ha='center', rotation=0)
+    upper_ax.set_xlabel("Population", fontsize=20, fontweight='bold', labelpad=14)
 
     # Calculate p-values and add brackets at specified y-coordinates
     p_values = calculate_p_values(file_names, cluster_names, column_index, condition_column_name, condition_value, total_score_condition)
@@ -85,7 +84,7 @@ def plot_data_with_p_values(ax, column_index, ylabel, is_first_plot, is_last_plo
             p_value = p_values[key]
             x1, x2 = tick_positions[cluster_names.index(cluster1)], tick_positions[cluster_names.index(cluster2)]
             add_p_value_brackets(ax, x1, x2, y_coord, p_value)
-    ax.set_xlabel('Cluster', fontsize=16, fontweight='bold', labelpad=10)  # Add x-axis label to the current subplot
+    ax.set_xlabel('Cluster', fontsize=20, fontweight='bold', labelpad=10)  # Add x-axis label to the current subplot
 
 # File names and cluster names
 file_names = ['c2.txt', 'c4.txt', 'c1.txt', 'c5.txt']
@@ -97,17 +96,19 @@ tick_labels = [int(cluster) for cluster in cluster_names]
 fig, axes = plt.subplots(1, 3, figsize=(20, 10), sharex=True, sharey=False, gridspec_kw={'wspace': 0.5})
 
 # Specify y-coordinates for each p-value line for each plot
-specified_y_coordinates_plot1 = [-618, -614, -610, -606, -602]
-specified_y_coordinates_plot2 = [1, 2.5, 4, 5.5, 7]
-specified_y_coordinates_plot3 = [1800, 1850, 1900, 1950, 2000]
+specified_y_coordinates_plot1 = [-618, -610, -600, -590, -580, -570]
+specified_y_coordinates_plot2 = [1, 4, 8, 12, 16, 20]
+specified_y_coordinates_plot3 = [1800, 1900, 2000, 2100, 2200, 2300]
 
 # Plot data for each subplot with specified y-coordinates for p-values
 plot_data_with_p_values(axes[0], 1, "Total Score (REU)", True, False, '', 'dG_separated', 0, -620, specified_y_coordinates_plot1)
-axes[0].set_ylim(-695, -595)  # Set y-limits for the first plot
+axes[0].set_ylim(-695, -561)  # Set y-limits for the first plot
 plot_data_with_p_values(axes[1], 6, "ΔG Binding (REU)", False, False, '', 'dG_separated', 0, -620, specified_y_coordinates_plot2)
-axes[1].set_ylim(-28, 8)  # Set y-limits for the second plot
+axes[1].set_ylim(-28, 24)  # Set y-limits for the second plot
 plot_data_with_p_values(axes[2], 9, "ΔSASA (Å²)", False, True, '', 'dG_separated', 0, -620, specified_y_coordinates_plot3)
-axes[2].set_ylim(900, 2050)  # Set y-limits for the third plot
-
+axes[2].set_ylim(900, 2390)  # Set y-limits for the third plot
+axes[1].set_yticks([0, -5, -10, -15, -20, -25])
+axes[0].set_yticks([-620, -640, -660, -680])
+axes[2].set_yticks([1800, 1600, 1400, 1200, 1000])
 plt.tight_layout()
 plt.show()
