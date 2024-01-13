c9397c50afa8b64a9c17d136b0f486f3340b888a
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-01-13 19:46:37 +0000

    contact_analysis.py

--- contact_analysis.py
+++ contact_analysis.py
@@ -57,6 +57,14 @@ for cluster_file in contact_files:
             matrix[i, j] = contact_frequency[cluster_file][pep_res][rec_res]
     heatmap_matrices[cluster_file] = (matrix, peptide_residues_sorted, receptor_residues_sorted)
 
+# Define frequency bar tick values for each plot
+frequency_ticks = {
+    'contacts_c2.txt': [1, 3, 5, 8, 10],
+    'contacts_c4.txt': [1, 250, 550, 800, 1093],
+    'contacts_c1.txt': [1, 200, 400, 600, 838],
+    'contacts_c5.txt': [1, 200, 450, 700, 947]
+}
+
 # Plotting with specific frequency bar ticks
 fig = plt.figure(figsize=(20, 20))  # Large figure size
 for idx, (cluster_file, (matrix, peptide_residues, receptor_residues)) in enumerate(heatmap_matrices.items()):
@@ -70,9 +78,9 @@ for idx, (cluster_file, (matrix, peptide_residues, receptor_residues)) in enumer
     
     heatmap = sns.heatmap(matrix, ax=ax, cmap=custom_cmap, annot=False, cbar=False, linewidths=.5, linecolor='black',
                 vmin=0, vmax=total_pdb_files_per_cluster[cluster_file], mask=matrix==0)
-    ax.set_title(titles[cluster_file], fontweight='bold', fontsize=18)
-    ax.set_ylabel('GnRH', fontweight='bold', fontsize=18) 
-    ax.set_xlabel('GnRH1R', fontweight='bold', fontsize=18)
+    ax.set_title(titles[cluster_file], fontweight='bold', fontsize=14)
+    ax.set_ylabel('GnRH', fontweight='bold', fontsize=14) 
+    ax.set_xlabel('GnRH1R', fontweight='bold', fontsize=14)
     ax.set_xticks(np.arange(len(receptor_residues)) + 0.5)
     ax.set_yticks(np.arange(len(peptide_residues)) + 0.5)
 
@@ -81,18 +89,18 @@ for idx, (cluster_file, (matrix, peptide_residues, receptor_residues)) in enumer
     ax.set_xticklabels(xtick_labels, rotation=90, fontsize=12)
     ax.set_yticklabels(peptide_residues, fontsize=12)
     cluster_title = titles[cluster_file]
-    ax.set_title(cluster_title, fontweight='bold', fontsize=18, color=title_colors[cluster_file])
+    ax.set_title(cluster_title, fontweight='bold', fontsize=14, color=title_colors[cluster_file])
   
     # Annotate boxes with frequency < 100 and rotate by 90 degrees for frequency > 100
     for i in range(len(peptide_residues)):
         for j in range(len(receptor_residues)):
             value = contact_frequency[cluster_file][peptide_residues[i]][receptor_residues[j]]
             if value < 100:
-                ax.text(j + 0.5, i + 0.5, str(value), ha='center', va='center', color='white', fontsize=6, fontweight='bold')
+                ax.text(j + 0.5, i + 0.5, str(value), ha='center', va='center', color='white', fontsize=8, fontweight='bold')
             elif value >= 100 and value <= 600:
-                ax.text(j + 0.5, i + 0.5, str(value), ha='center', va='center', color='white', fontsize=6, fontweight='bold', rotation=90)
+                ax.text(j + 0.5, i + 0.5, str(value), ha='center', va='center', color='white', fontsize=8, fontweight='bold', rotation=90)
             elif value > 600:
-                ax.text(j + 0.5, i + 0.5, str(value), ha='center', va='center', color='white', fontsize=6, fontweight='bold', rotation=90)
+                ax.text(j + 0.5, i + 0.5, str(value), ha='center', va='center', color='white', fontsize=8, fontweight='bold', rotation=90)
 
     # Removing the extra box
     ax.spines['top'].set_visible(False)
@@ -100,16 +108,14 @@ for idx, (cluster_file, (matrix, peptide_residues, receptor_residues)) in enumer
     ax.spines['bottom'].set_visible(True)
     ax.spines['left'].set_visible(False)
     
-    # Adding a custom colorbar to each plot
+    # Adding a custom colorbar to each plot with specified frequency ticks
     max_freq = total_pdb_files_per_cluster[cluster_file]
     sm = ScalarMappable(cmap=custom_cmap, norm=plt.Normalize(vmin=0, vmax=max_freq))
     sm.set_array([])
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="2.5%", pad=0.04)
-    cbar = plt.colorbar(sm, cax=cax)
-    cbar.set_label('Frequency', fontweight='bold', fontsize=12)
-    cbar.set_ticks([0, max_freq / 4, max_freq / 2, 3 * max_freq / 4, max_freq])
-    cbar.set_ticklabels(['0', f'{max_freq // 4}', f'{max_freq // 2}', f'{3 * max_freq // 4}', f'{max_freq}'])
+    cbar = plt.colorbar(sm, cax=cax, ticks=frequency_ticks[cluster_file])
+    cbar.set_label('Population', fontweight='bold', fontsize=12)
 
-plt.subplots_adjust(hspace=0.5, wspace=0.25)
+plt.subplots_adjust(hspace=0.5, wspace=0.2)
 plt.show()
