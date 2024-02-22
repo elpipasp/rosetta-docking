35d925ee1c2ee161f04ab498dd21b9875c0d33d1
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-02-22 19:57:58 +0000

    find_contacts.py

--- find_contacts.py
+++ find_contacts.py
@@ -5,14 +5,14 @@ def find_contacts(pdb_file, chain_gpcr, chain_peptide, distance_threshold=5.0):
     parser = PDB.PDBParser(QUIET=True)
     structure = parser.get_structure('struct', pdb_file)
 
-    # Function to choose the right atom for distance calculation
+    #function to choose the right atom for distance calculation
     def get_representative_atom(residue):
         if residue.get_resname() == 'GLY':
             return residue['CA'] if 'CA' in residue else None
         else:
             return residue['CB'] if 'CB' in residue else residue.get('CA', None)
 
-    # Extract representative atoms from GPCR and peptide
+    #extract representative atoms from GPCR and peptide
     rep_atoms_gpcr = []
     rep_atoms_peptide = []
     for model in structure:
@@ -30,12 +30,12 @@ def find_contacts(pdb_file, chain_gpcr, chain_peptide, distance_threshold=5.0):
                         if atom:
                             rep_atoms_peptide.append(atom)
 
-    # Check if there are atoms to work with
+    #check if there are atoms to work with
     if not rep_atoms_gpcr or not rep_atoms_peptide:
         print(f"No valid atoms found in {pdb_file} for contact analysis.")
         return []
-
-    # Finding contacts
+        
+    #finding contacts
     ns = PDB.NeighborSearch(rep_atoms_gpcr)
     contacts = []
     for atom in rep_atoms_peptide:
@@ -44,14 +44,11 @@ def find_contacts(pdb_file, chain_gpcr, chain_peptide, distance_threshold=5.0):
             contacts.append((atom, close_atom))
 
     return contacts
-
-# Parameters
-source_directory = '.'  # Current directory
+source_directory = '.'  #current directory
 output_filename = 'contacts_c4.txt'
-chain_gpcr = 'A'  # Replace with the chain ID of the GPCR
-chain_peptide = 'P'  # Replace with the chain ID of the peptide
-
-# Process each PDB file and write contacts to file
+chain_gpcr = 'A'  # replace with the chain ID of the GPCR
+chain_peptide = 'P'  #replace with the chain ID of the peptide
+#process each PDB file and write contacts to file
 with open(output_filename, 'w') as output_file:
     for file in os.listdir(source_directory):
         if file.endswith(".pdb") and "c.4." in file:
@@ -63,5 +60,4 @@ with open(output_filename, 'w') as output_file:
                 res2 = atom2.get_parent()
                 output_file.write(f"{res1.get_resname()} {res1.id[1]} {atom1.get_name()} - {res2.get_resname()} {res2.id[1]} {atom2.get_name()}\n")
             output_file.write("\n")
-
 print("Contact analysis complete. Results are in 'contacts_c4.txt'")
