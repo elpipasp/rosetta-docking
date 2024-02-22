652c56c17152a9e45acba8f7ee371d01d999da90
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-02-22 20:00:11 +0000

    get_cluster_data.py

--- get_cluster_data.py
+++ get_cluster_data.py
@@ -1,27 +1,18 @@
 import re
 from collections import defaultdict
 
-# Input file path
 input_file_path = 'cluster_InterfaceAnalyzer.csv'
-
-# Define a regular expression pattern to extract cluster information from the description column
 cluster_pattern = re.compile(r'c\.(\d+)\.(\d+)')
-
-# Dictionary to store data for each cluster
 cluster_data = defaultdict(list)
 
-# Read data from the input file
 with open(input_file_path, 'r') as input_file:
     lines = input_file.readlines()
 
-# Extract and store the header line
-header_line = lines[1]  # Assuming the header line is at index 1
-header_line = header_line.replace('\n', '')  # Remove the newline character
+#extract and store the header line
+header_line = lines[1]
+header_line = header_line.replace('\n', '')
 
-# Initialize variables to track the current cluster
 current_cluster = None
-
-# Parse data and store it in the dictionary
 for line in lines:
     if line.startswith('SCORE:'):
         match = cluster_pattern.search(line)
@@ -34,7 +25,6 @@ for line in lines:
                 cluster_data[current_cluster].append(header_line)
             cluster_data[current_cluster].append(line)
 
-# Write data for each cluster to a separate file
 for cluster_number, data in cluster_data.items():
     output_file_path = f'c{cluster_number}.dat'
     with open(output_file_path, 'w') as output_file:
