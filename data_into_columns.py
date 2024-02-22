1d2196744d29ca5e73b5705fb4b3b66caa0c4e1d
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-02-22 19:54:50 +0000

    data_into_columns.py

--- data_into_columns.py
+++ data_into_columns.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-# List of data files
+#list of data files
 data_files = [
     'c4.dat',
     'c1.dat',
@@ -26,13 +26,10 @@ data_files = [
     'c20.dat',
 ]
 
-# Process each data file
+#process each data file
 for file_name in data_files:
-    # Read the data file
     data = pd.read_csv(file_name, delim_whitespace=True)
-
-    # Save the data as a space-separated file with a .txt extension
+    #save the data as a space-separated file with a .txt extension
     output_file_path = f'{file_name.split(".")[0]}.txt'
     data.to_csv(output_file_path, sep=' ', index=False)
-
     print(f'{file_name} converted to {output_file_path}')
